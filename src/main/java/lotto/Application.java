7e6ba17ab4d7fa4be0e44ac961a5494be3956e8a
sun7898 <jhs15767@gmail.com>
2025-11-03 19:47:06 +0900

    feat: 로또 번호 생성 기능 LottoService로 이동 및 Application 호출

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,13 +1,23 @@
 package lotto;
 
+import lotto.service.LottoService;
 import lotto.util.InputValidator;
+import lotto.domain.Lotto;
+
+import java.util.List;
 
 public class Application {
 
     public static void main(String[] args) {
-        // 기존 구입 금액 입력 코드 삭제 → InputValidator로 이동
         int purchaseAmount = InputValidator.readPurchaseAmount();
+        int lottoCount = purchaseAmount / 1000;
+        System.out.println(lottoCount + "개를 구매했습니다.");
 
-        // TODO: LottoService로 로또 생성, 당첨 판정, 통계 출력
+        List<Lotto> lottos = LottoService.generateLottos(lottoCount);
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto.getNumbers());
+        }
+
+        // 다음 단계: 당첨 번호 입력 및 당첨 판정
     }
 }
---
+++ src/main/java/lotto/service/LottoService.java
@@ -0,0 +1,21 @@
+package lotto.service;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.domain.Lotto;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class LottoService {
+
+    public static List<Lotto> generateLottos(int count) {
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            Collections.sort(numbers);
+            lottos.add(new Lotto(numbers));
+        }
+        return lottos;
+    }
+}
